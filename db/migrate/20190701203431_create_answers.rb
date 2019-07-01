b4125e3ce07851abff2fe7615a485391b9780459
ditmarbrynza <ditmarwork@gmail.com>
2019-07-02 01:09:01 +0300

    add some migrations

--- db/migrate/20190701203431_create_answers.rb
+++ db/migrate/20190701203431_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.string :body
+      t.string :correct
       t.integer :question_id
 
       t.timestamps
