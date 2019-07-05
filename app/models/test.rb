6b00533497a029697de44329bd7e32f1c0531abe
ditmarbrynza <ditmarwork@gmail.com>
2019-07-06 02:01:22 +0300

    add  method all_test_titles for category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.all_test_titles(category)
+    self.joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order("title ASC")
+  end
+
 end
