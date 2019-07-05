8bea8a8aa64c9ff7c3b308d563577774ec1a0fa6
ditmarbrynza <ditmarwork@gmail.com>
2019-07-05 23:18:38 +0300

    add tests data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+test = Test.create(title: "Animation", level: 1, category_id: 1)
+test = Test.create(title: "Flexbox", level: 2, category_id: 1)
+test = Test.create(title: "Grid", level: 3, category_id: 1)
+
+test = Test.create(title: "DOM", level: 1, category_id: 2)
+test = Test.create(title: "Events", level: 2, category_id: 2)
+test = Test.create(title: "ES6", level: 3, category_id: 2)
+
+test =  Test.create(title: "Props", level: 1, category_id: 3)
+test =  Test.create(title: "Components", level: 2, category_id: 3)
+test =  Test.create(title: "Redux", level: 3, category_id: 3)
