edf56c5c2ab4c0e908238d2d5fa7f42ec56e0626
ditmarbrynza <ditmarwork@gmail.com>
2019-07-06 20:01:27 +0300

    fix completed_tests logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def completed_tests(level)
-    Test.joins('INNER JOIN complited_tests ON tests.id = complited_tests.test_id').where("complited_tests.user_id = #{self.id}", level: level)
+    Test.joins('INNER JOIN complited_tests ON tests.id = complited_tests.test_id').where("complited_tests.user_id = #{self.id}").where("level = ?", level)
   end
 
 end
