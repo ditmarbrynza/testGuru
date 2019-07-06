303f4db170fe2e395f8e269e4b9c0297fd1c7643
ditmarbrynza <ditmarwork@gmail.com>
2019-07-06 17:47:48 +0300

    add some fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
 
   def self.all_test_titles(category)
-    self.joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order("title ASC")
+    self.joins("JOIN categories ON tests.category_id = categories.id").where(categories: { title: category} ).order("categories.title DESC").pluck("tests.title")
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def completed_tests(level)
-    User.select("tests.title").joins("INNER JOIN complited_tests ON complited_tests.user_id = users.id INNER JOIN tests ON tests.id = complited_tests.test_id").where("users.id = #{self.id} AND level = ?", level)
+    Test.joins('INNER JOIN complited_tests ON tests.id = complited_tests.test_id').where("complited_tests.user_id = #{self.id}", level: level)
   end
 
 end
