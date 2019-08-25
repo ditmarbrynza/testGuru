e585069b9c9f3867e9e5021a39f9dd3ddb59620d
ditmarbrynza <ditmarwork@gmail.com>
2019-08-25 14:37:03 +0300

    add some features

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,55 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_question, only: %i[show]
+  skip_before_action :verify_authenticity_token
+  before_action :find_test, only: %i[index]
+
+  before_action :find_question, only: %i[show destroy]
+  before_action :find_all_question, only: %i[index]
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render json: { questions: @test.questions }
+  end
 
   def show
-    render inline: '<%= @question.title %>'
+    render inline: '<%= @question.body %>'
+  end
+
+  def create
+    question = Question.create(question_params)
+    render plain: question.inspect
+  end
+
+  def destroy
+    @question.destroy
+    render inline: 'Question was deleted'
+  end
+
+  def new
+
   end
 
   private
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_all_question
+    @questions = Question.all
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question was not Found"
+  end
+
+  def question_params
+    params.require(:questions).permit(:body, :test_id)
   end
 
 end
