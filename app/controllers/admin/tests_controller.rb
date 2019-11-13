ef149b7b12221443bbbcc5404bfa454ea2b68334
ditmarbrynza <ditmarwork@gmail.com>
2019-11-14 02:30:46 +0300

    add some to 11 lesson

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,91 @@
+class Admin::TestsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :find_test, only: %i[show edit update destroy start]
+  after_action :send_log_message
+  around_action :log_execute_time
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+
+  end
+
+  def update
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def search
+    result = ["Class: #{params.class}", "Parameters: #{params.inspect}"]
+    render plain: result.join("\n")
+  end
+
+  def start
+    @user = current_user
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end 
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def find_all_test
+    @all_tests = Test.all
+  end
+
+  def send_log_message
+    logger.info("Action [#{action_name}] was finished")
+  end
+
+  def log_execute_time
+    start = Time.now
+    yield
+    finish = Time.now - start
+
+    logger.info("Execution time: #{finish * 1000}ms")
+  end
+
+  def rescue_with_test_not_found
+    render plain: "Test was not Found"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'tests#index'
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
@@ -11,7 +9,7 @@ Rails.application.routes.draw do
 
   delete :logout, to: 'sessions#destroy'
 
-  resources :tests do
+  resources :test, only: :index do
     resources :questions, shallow: true, except: :index do 
       resources :answers, shallow: true, except: :index
     end
@@ -21,37 +19,14 @@ Rails.application.routes.draw do
     end
   end
 
-  #GET /test_passages/101/result
   resources :test_passages, only: %i[show update] do
     member do
       get :result
     end 
   end
 
-  # resource :account
-  # resource :account, controller: :account
-
-  # resources :tests, only: %i[index show]
-
-  # resources :tests do
-  #   resources :questions, shallow: true
-  # end
-
-  # resources :tests do
-  #   get :beginner, on: :collection
-
-  #   member do
-  #     post :start
-  #   end
-  # end
-
-  # get '/tests', to: 'tests#index'
-  # get '/tests(/:id)', to: 'tests#show', as: :test
-  # get '/tests/:id/start', to: 'tests#start'
-  # get  '/tests/new', to: 'tests#new'
-  # get '/tests/:id/start', to: 'tasks#start'
-
-  # match '/tests/:id', to: 'tests#create', via: %i[post put ]
-
+  namespace :admin do
+    resources :tests
+  end
 
 end
