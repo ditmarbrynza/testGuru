880750b1f0af24774d1763c17c9350898767b62f
ditmarbrynza <ditmarwork@gmail.com>
2019-11-30 19:32:46 +0300

    fix answers_correct type and default value

--- db/migrate/20190701203431_create_answers.rb
+++ db/migrate/20190701203431_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.string :body
-      t.string :correct
+      t.boolean :correct, default: false
       t.integer :question_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_22_202556) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false
