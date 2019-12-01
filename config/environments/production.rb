e36ea33cc2f1458b49477503338bae4a41852d25
ditmarbrynza <ditmarwork@gmail.com>
2019-12-02 01:31:03 +0300

    add app name to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testGuru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'HEROKU-APP-NAME.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'sleepy-springs-14098.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                    'smtp.gmail.com',
